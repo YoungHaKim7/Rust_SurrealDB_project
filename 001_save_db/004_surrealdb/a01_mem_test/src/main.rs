685b25c02b7899e4c6d9523de5f8f61bc6a6429a
GYoung <ytok1108@gmail.com>
2026-02-24 18:02:42 +0900

    surrealDB SDK

---
+++ 001_save_db/004_surrealdb/a01_mem_test/src/main.rs
@@ -0,0 +1,29 @@
+
+use surrealdb::engine::remote::ws::Ws;
+use surrealdb::opt::auth::Root;
+use surrealdb::Surreal;
+
+#[tokio::main]
+async fn main() -> surrealdb::Result<()> {
+
+    // Connect to the server
+    let db = Surreal::new::<Ws>("127.0.0.1:8000").await?;
+
+    // Signin as a namespace, database, or root user
+    db.signin(Root {
+        username: "root".to_string(),
+        password: "secret".to_string(),
+    })
+    .await?;
+
+    // Select a specific namespace / database
+    db.use_ns("test").use_db("test").await?;
+
+    let some_queries = db.query("
+        RETURN 9; 
+        RETURN 10; 
+        SELECT * FROM { is: 'Nice database' };
+    ").await?;
+    dbg!(some_queries);
+    Ok(())
+}
