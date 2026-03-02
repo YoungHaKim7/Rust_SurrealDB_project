287aa0e0bf8946334d4015d805cbfd585af4ad80
GYoung <ytok1108@aol.com>
2026-03-02 16:32:42 +0900

    input sql

---
+++ 001_save_db/003_surrealkv/a03_inputsql/README.md
@@ -0,0 +1,11 @@
+# Result
+
+
+# Result
+
+
+```bash
+
+
+```
+
---
+++ 001_save_db/003_surrealkv/a03_inputsql/src/main.rs
@@ -0,0 +1,26 @@
+use surrealdb::Surreal;
+use surrealdb::engine::remote::ws::Ws;
+use surrealdb::opt::auth::Root;
+
+#[tokio::main]
+async fn main() -> surrealdb::Result<()> {
+    // Connect to the server
+    let db = Surreal::new::<Ws>("127.0.0.1:8000").await?;
+
+    // Signin as a namespace, database, or root user
+    db.signin(Root {
+        username: "root".to_string(),
+        password: "root".to_string(),
+    })
+    .await?;
+
+    // Select a specific namespace / database
+    db.use_ns("test").use_db("test").await?;
+
+    let sql_content = std::fs::read_to_string("./assets/input_sql_file/input_db_data.surql")
+        .expect("input SQL file, error");
+    let some_queries = db.query(sql_content).await?;
+
+    dbg!(some_queries);
+    Ok(())
+}
