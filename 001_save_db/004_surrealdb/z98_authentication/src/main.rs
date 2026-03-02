5ebb023e16794212f499aa2f24147e2541f1f128
GYoung <ytok1108@aol.com>
2026-03-02 23:14:26 +0900

    https://surrealdb.com/docs/surrealdb/security/authentication

---
+++ 001_save_db/004_surrealdb/z98_authentication/src/main.rs
@@ -0,0 +1,58 @@
+use surrealdb::{Surreal, engine::remote::ws::Ws, opt::auth::Record, opt::auth::Root};
+
+#[tokio::main]
+async fn main() -> surrealdb::Result<()> {
+    let db = Surreal::new::<Ws>("127.0.0.1:8000").await?;
+
+    db.signin(Root {
+        username: "root".to_string(),
+        password: "root".to_string(),
+    })
+    .await?;
+
+    // Select the namespace/database to use
+    // Note: For in-memory databases, no authentication is required
+    db.use_ns("namespace").use_db("database").await?;
+
+    // Define the user record access
+    let surql = r#"
+    DEFINE TABLE user SCHEMAFULL
+    	PERMISSIONS
+    		FOR select, update, delete WHERE id = $auth.id;
+
+    DEFINE FIELD name ON user TYPE string;
+    DEFINE FIELD email ON user TYPE string ASSERT string::is_email($value);
+    DEFINE FIELD password ON user TYPE string;
+
+    DEFINE INDEX email ON user FIELDS email UNIQUE;
+
+    DEFINE ACCESS user ON DATABASE TYPE RECORD
+    	SIGNIN (
+    		SELECT * FROM user WHERE email = $email AND crypto::argon2::compare(password, $password)
+    	)
+    	SIGNUP (
+    		CREATE user CONTENT {
+    			name: $name,
+    			email: $email,
+    			password: crypto::argon2::generate($password)
+    		}
+    	);
+"#;
+    db.query(surql).await?.check()?;
+
+    // Sign a user up using named parameters
+    db.signup(Record {
+        namespace: "namespace".to_string(),
+        database: "database".to_string(),
+        access: "user".to_string(),
+        params: serde_json::json!({
+            "name": "John Doe",
+            "email": "john.doe@example.com",
+            "password": "1234"
+        }),
+    })
+    .await?;
+
+    println!("User signup successful!");
+    Ok(())
+}
